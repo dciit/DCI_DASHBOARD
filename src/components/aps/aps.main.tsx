64c59978afd6cccffb9b39709ac422eecb81947a
github-devmaker <peerapong.kiadtipongwiriya@gmail.com>
2024-06-20 13:53:47 +0700

    init data main and final [Scroll]

--- src/components/aps/aps.main.tsx
+++ src/components/aps/aps.main.tsx
@@ -8,7 +8,8 @@ import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import moment from 'moment';
 function ApsMain() {
     let dateformat = 'DD/MM/YYYY';
-    const [data, setData] = useState<ApsMainProps[]>([]);
+    const [dataMain, setDataMain] = useState<ApsMainProps[]>([]);
+    const [dataFinal, setDataFinal] = useState<ApsMainProps[]>([]);
     const [load, setLoad] = useState<boolean>(true);
     const [date, setDate] = useState<string>(moment().format(dateformat));
     useEffect(() => {
@@ -28,7 +29,8 @@ function ApsMain() {
         try {
             setLoad(true);
             const res = await ApsMainGetData(moment(date, dateformat).format('YYYYMMDD'));
-            setData(res);
+            setDataMain(res.filter((o: ApsMainProps) => o.subline == 'ASSEMBLY LINE4 (SCR)'))
+            setDataFinal(res.filter((o: ApsMainProps) => o.subline == 'FINAL-ASSEMBLY LINE4 (SCR)'))
             setLoad(false);
         } catch (error: any) {
             alert(error.message)
@@ -38,104 +40,88 @@ function ApsMain() {
         fetchData();
     }, [date])
     return (
-        <div>
-            <div className='flex justify-center py-2 cursor-pointer select-none'>
-                <div onClick={() => setDate((prev) => moment(prev, dateformat).add('days', -1).format(dateformat))}><ChevronLeftIcon className='hover:scale-110 transition-all duration-300' /></div>
-                <div>{date}</div>
-                <div onClick={() => setDate((prev) => moment(prev, dateformat).add('days', 1).format(dateformat))}> <ChevronRightIcon className='hover:scale-110 transition-all duration-300' /></div>
-            </div>
-            <div className='grid grid-cols-2'>
-                <div className='col-span-1'>
-                    <table className='w-full'>
-                        <thead>
-                            <tr>
-                                <td className='border text-center ' colSpan={7} >MAIN</td>
-                            </tr>
-                            <tr>
-                                <td className='border text-center'>SEQUENCE</td>
-                                <td className='border text-center'>WCNO</td>
-                                <td className='border text-center'>MODEL CODE	</td>
-                                <td className='border text-center'>PartNo</td>
-                                <td className='border text-center'>Plan</td>
-                                <td className='border text-center'>Actual</td>
-                                <td className='border text-center'> Remark</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                load ? <tr><td className='border' colSpan={14}><div className='flex flex-col justify-center items-center py-3'>
-                                    <CircularProgress />
-                                    <span>กำลังโหลดข้อมูล</span>
-                                </div></td></tr> :
-                                    (
-                                        data.filter((o: ApsMainProps) => o.subline == 'ASSEMBLY LINE4 (SCR)').length == 0 ? <div>ไม่พบข้อมูล</div> :
-                                            data.filter((o: ApsMainProps) => o.subline == 'ASSEMBLY LINE4 (SCR)').map((item: ApsMainProps, index: number) => {
-                                                return <tr key={index}>
-                                                    <td className='border text-center'>{index + 1}</td>
-                                                    <td className='border text-center'>{item.p_wcno}</td>
-                                                    <td className='border text-center'>{item.p_modelcode}</td>
-                                                    <td className='border text-center'>{item.p_model}</td>
-                                                    <td className='border text-center'>{item.p_planqty}</td>
-                                                    <td className='border text-center'>-</td>
-                                                    <td className='border text-center'>
-                                                        <div className='flex justify-center bg-red-50 border border-red-300 w-fit rounded-md px-3 text-red-500 cursor-pointer select-none shadow-md'>
-                                                            <EditOutlinedIcon />
-                                                            <div>หมายเหตุ</div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            })
-                                    )
-                            }
-                        </tbody>
-                    </table>
-                </div>
-                <div className='col-span-1'>
-                    <table className='w-full'>
-                        <thead>
-                            <tr>
-                                <td className='border text-center ' colSpan={7} >FINAL</td>
-                            </tr>
-                            <tr>
-                                <td className='border text-center'>SEQUENCE</td>
-                                <td className='border text-center'>WCNO</td>
-                                <td className='border text-center'>MODEL CODE	</td>
-                                <td className='border text-center'>PartNo</td>
-                                <td className='border text-center'>Plan</td>
-                                <td className='border text-center'>Actual</td>
-                                <td className='border text-center'> Remark</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                load ? <tr><td className='border' colSpan={14}><div className='flex flex-col justify-center items-center py-3'>
-                                    <CircularProgress />
-                                    <span>กำลังโหลดข้อมูล</span>
-                                </div></td></tr> :
-                                    (
-                                        data.filter((o: ApsMainProps) => o.subline == 'FINAL-ASSEMBLY LINE4 (SCR)').length == 0 ? <div>ไม่พบข้อมูล</div> :
-                                            data.filter((o: ApsMainProps) => o.subline == 'FINAL-ASSEMBLY LINE4 (SCR)').map((item: ApsMainProps, index: number) => {
-                                                return <tr key={index}>
-                                                    <td className='border text-center'>{index + 1}</td>
-                                                    <td className='border text-center'>{item.p_wcno}</td>
-                                                    <td className='border text-center'>{item.p_modelcode}</td>
-                                                    <td className='border text-center'>{item.p_model}</td>
-                                                    <td className='border text-center'>{item.p_planqty}</td>
-                                                    <td className='border text-center'>-</td>
-                                                    <td className='border text-center'>
-                                                        <div className='flex justify-center bg-red-50 border border-red-300 w-fit rounded-md px-3 text-red-500 cursor-pointer select-none shadow-md'>
-                                                            <EditOutlinedIcon />
-                                                            <div>หมายเหตุ</div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            })
-                                    )
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+        <div className='p-3' id='aps'>
+            <table className='w-full'>
+                <thead>
+                    <tr>
+                        <td className='border text-center' colSpan={14}>
+                            <div className='flex items-center justify-center'>
+                                <span>Scroll</span>
+                                <div className='flex justify-center py-2 cursor-pointer select-none'>
+                                    <div onClick={() => setDate((prev) => moment(prev, dateformat).add('days', -1).format(dateformat))}><ChevronLeftIcon className='hover:scale-110 transition-all duration-300' /></div>
+                                    <div>{date}</div>
+                                    <div onClick={() => setDate((prev) => moment(prev, dateformat).add('days', 1).format(dateformat))}> <ChevronRightIcon className='hover:scale-110 transition-all duration-300' /></div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='border text-center py-2' colSpan={7}>Main</td>
+                        <td className='border text-center py-2' colSpan={7}>Final</td>
+                    </tr>
+                    <tr>
+                        <td className='border text-center py-2'>SEQUENCE</td>
+                        <td className='border text-center py-2'>WCNO</td>
+                        <td className='border text-center py-2'>MODEL CODE	</td>
+                        <td className='border text-center py-2'>PartNo</td>
+                        <td className='border text-center py-2'>Plan</td>
+                        <td className='border text-center py-2'>Actual</td>
+                        <td className='border text-center py-2'> Remark</td>
+                        <td className='border text-center py-2'>SEQUENCE</td>
+                        <td className='border text-center py-2'>WCNO</td>
+                        <td className='border text-center py-2'>MODEL CODE	</td>
+                        <td className='border text-center py-2'>PartNo</td>
+                        <td className='border text-center py-2'>Plan</td>
+                        <td className='border text-center py-2'>Actual</td>
+                        <td className='border text-center py-2'> Remark</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        load ? <tr><td className='border' colSpan={14}><div className='flex flex-col justify-center items-center py-3'>
+                            <CircularProgress />
+                            <span>กำลังโหลดข้อมูล</span>
+                        </div></td></tr> : [...Array(dataMain.length > dataFinal.length ? dataMain.length : dataFinal.length)].map((_: any, index: number) => {
+                            return <>
+                                <tr key={index}>
+                                    {
+                                        typeof dataMain[index] !== 'undefined' ? <>
+                                            <td className='border text-center'>{index + 1}</td>
+                                            <td className='border text-center'>{dataMain[index].p_wcno}</td>
+                                            <td className='border text-center'>{dataMain[index].p_modelcode}</td>
+                                            <td className='border text-center'>{dataMain[index].p_model}</td>
+                                            <td className='border text-center'>{dataMain[index].p_planqty}</td>
+                                            <td className='border text-center'>-</td>
+                                            <td className='border text-center'>
+                                                <div className='flex justify-center bg-red-50 border border-red-300 w-fit rounded-md px-3 text-red-500 cursor-pointer select-none shadow-md'>
+                                                    <EditOutlinedIcon />
+                                                </div>
+                                            </td>
+                                        </> : <>
+                                            <td colSpan={7} className='border '></td>
+                                        </>
+                                    }
+                                    {
+                                        typeof dataFinal[index] !== 'undefined' && <>
+                                            <td className='border text-center'>{index + 1}</td>
+                                            <td className='border text-center'>{dataFinal[index].p_wcno}</td>
+                                            <td className='border text-center'>{dataFinal[index].p_modelcode}</td>
+                                            <td className='border text-center'>{dataFinal[index].p_model}</td>
+                                            <td className='border text-center'>{dataFinal[index].p_planqty}</td>
+                                            <td className='border text-center'>-</td>
+                                            <td className='border text-center'>
+                                                <div className='flex justify-center bg-red-50 border border-red-300 w-fit rounded-md px-3 text-red-500 cursor-pointer select-none shadow-md'>
+                                                    <EditOutlinedIcon />
+                                                </div>
+                                            </td>
+                                        </>
+                                    }
+                                </tr>
+                            </>
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
